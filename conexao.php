9a43d506f18a3a450dc90a622709632838bea0bd
GLeoni17 <gabriel.lduarte@outlook.com>
2020-12-15 01:02:00 -0300

    Conexao ao banco de dados online

--- conexao.php
+++ conexao.php
@@ -1,7 +1,14 @@
 <?php
-    $host = "localhost";
+    /*$host = "localhost";
     $user = "root";
     $password = "usbw";
-    $bd = "trabalho_pratico";
+    $bd = "trabalho_pratico";*/
+
+    $local = "db4free.net:3306";
+    $user = "gleoni2";
+    $senha = "usbwusbw";
+    $bd = "projeto_csgo";
+
+
     $con = mysqli_connect($host, $user, $password, $bd) or die("Erro ao conectar ao banco de dados.");
 ?>
